af822a4e87ef25ad235c575a346d598f968d05f9
Rashaad Sidique <rsidique@gmail.com>
2019-05-03 12:02:14 -0700

    handle orientation updates asynchronously

--- Sources/NextLevel.swift
+++ Sources/NextLevel.swift
@@ -3109,7 +3109,7 @@ extension NextLevel {
     
     @objc internal func deviceOrientationDidChange(_ notification: NSNotification) {
         if self.automaticallyUpdatesDeviceOrientation {
-            self._sessionQueue.sync {
+            self._sessionQueue.async {
                 self.updateVideoOrientation()
             }
         }
